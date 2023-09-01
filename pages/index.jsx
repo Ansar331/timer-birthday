7e203e1aadd94828ea426c4133d328bf1db5b606
Ansar331 <95215784+Ansar331@users.noreply.github.com>
2023-09-01 19:05:06 +0600

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -47,25 +47,8 @@ export default function Home() {
   }, []);
 
   return (
-    <div style={{ textAlign: 'center', padding: '20px' }}>
-      <h1>Обратный Отсчет До Дня Рождения Аиши!!!</h1>
-      <div style={{ fontSize: '24px' }}>
-        <p>Оставшееся время до 28 Августа:</p>
-        <p>
-          {timeLeft.days} Дней, {timeLeft.hours} Часов, {timeLeft.minutes} Минут, {timeLeft.seconds} Секунд
-        </p>
-      </div>
-      {showConfetti && <Confetti />} {/* Отображайте конфетти, если showConfetti равно true */}
-      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-      {showConfetti && <h1>С Днем Рождения Аиша!!!&#128525;&#128536;&#129321;&#129392;&#128525;</h1>}
-      <style jsx>{`
-        h1 {
-          font-size: 36px;
-        }
-        p {
-          margin: 10px 0;
-        }
-      `}</style>
+    <div>
+      Сережа пидарас)
     </div>
   );
 }
